45c9b22e10487370dab7021d6380521fff6d1d18
shaem <towqirahmedshaem@gmail.com>
2017-03-17 00:58:40 +0600

    Add files via upload

---
+++ ARDUINO_TEMPERATURE_SENSOR_LM35.ino
@@ -0,0 +1,26 @@
+/*
+ Developer : Towqir Ahmed Shaem
+ Institute : Appplied Physics, Electronics and Communication Engineering, Chittagong University.
+ Email : towqirahmedshaem@gmail.com
+ */
+int value;
+int tempPin =A0;
+void setup()
+{
+ pinMode(tempPin,INPUT);
+ Serial.begin(9600);
+}
+void loop()
+{
+value = analogRead(tempPin);
+float mv =(5.0 * 1000.0 * value ) / 1024;    // It is the equation to find the value in mV. Here 5 is our  given voltage.
+float celcius = mv/10;                       // 10mV is equal to 1 degree Celcius
+float farenhite  = (celcius*9)/5 + 32;
+Serial.print("TEMPRATURE = ");
+Serial.print(celcius);
+Serial.print("*C    ");
+Serial.print(farenhite );
+Serial.print("*F");
+Serial.println();
+delay(1000);
+}
